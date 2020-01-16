db6e8242528bf7d4b54db56f3da81efd20270dae
Trey Suiter <trey.suiter@gmail.com>
2020-01-16 14:28:51 -0600

    arrays created for students and exercises

--- instructor.py
+++ instructor.py
@@ -1,12 +1,13 @@
 class Instructor:
 
-    def __init__(self, new_firstName, new_lastName, new_currentCohort, new_specialty):
+    def __init__(self, new_firstName, new_lastName, new_slackName, new_currentCohort, new_specialty):
 
         self.firstName = new_firstName
         self.lastName = new_lastName
+        self.slackName = new_slackName
         self.currectCohort = new_currentCohort
         self.specialty = new_specialty
 
     def assign_exercise(self, student, exercise):
 
-        student.currectExercises.apppend(exercise)
+        student.currectExercises.append(exercise)
