f0eecaed24b5650cec7aa8986231bd8251514d39
Trey Suiter <trey.suiter@gmail.com>
2020-01-20 21:29:19 -0600

    inherited class nssperson implemented; working with super init

---
+++ nssPerson.py
@@ -0,0 +1,6 @@
+class NSSPerson():
+    def __init__(self, new_firstName, new_lastName, new_slackName, new_cohortName):
+        self.firstName = new_firstName
+        self.lastName = new_lastName
+        self.slackName = new_slackName
+        self.currectCohort = new_cohortName
